d06f73e48da044633fe074f28f39f054e08defda
Jack Aakash <jackaakash4@gmail.com>
2025-03-04 00:32:13 +0545

    Performed generic type in a function

--- generic.go
+++ generic.go
@@ -1,18 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 //learning generic in go
 //sum
 //input: 1, 2 or 1.5, 1.5
 //output: 3 or 3.00
 
-func sum(a, b int) int {
+//creating a custom type
+// type customType2 interface{
+// 	constraints.Ordered | []byte | []rune
+// }
+
+type customType interface {
+	int | int32 | float32 | float64
+}
+
+func sum[T customType](a, b T) T {
 	return a + b
 }
 
 func main() {
-	result := sum(1, 2)
+	result := sum(1.5, 1.5)
 
 	fmt.Println("The sum is ", result)
 }
