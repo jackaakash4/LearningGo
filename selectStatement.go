b29a59ed48c5fb124b4c41a072eb2ca0b216ab45
Jack Aakash <jackaakash4@gmail.com>
2025-03-12 11:30:39 +0545

    Learned: Select statement
    The select statement allows us to wait on multiple channel operations

---
+++ selectStatement.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+    "fmt"
+    "time"
+)
+
+func main() {
+    ch1 := make(chan string)
+    ch2 := make(chan string)
+
+    go func() {
+        time.Sleep( 1 * time.Second)
+        ch1 <- "Hello"
+    }()
+
+    go func() {
+        time.Sleep(2 * time.Second)
+        ch2 <- "From Jack"
+    }()
+
+    for i := 0; i < 2; i++ {
+        select {
+        case msg := <-ch1:
+            fmt.Println(msg)
+        case msg := <-ch2:
+            fmt.Println(msg)
+        }
+    }
+}
