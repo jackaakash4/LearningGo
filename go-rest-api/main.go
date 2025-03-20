19946fc0d6960044ee27c3c3864f07498731fa17
Jack Aakash <jackaakash4@gmail.com>
2025-03-20 13:33:16 +0545

    Created: A rest api is created
    Building an api

---
+++ go-rest-api/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+    "log"
+    "io"
+)
+
+func main() {
+
+    resp, err := http.Get("https://example.com")
+
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    defer resp.Body.Close()
+
+    body, err := io.ReadAll(resp.Body)
+
+    fmt.Println("Respose body: ", body)
+}
+
