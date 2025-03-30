e8cb2bebf7f65b16d681adde3d77ab7780898bdb
Jack Aakash <jackaakash4@gmail.com>
2025-03-30 23:22:42 +0545

    Handled the response

--- go-rest-api/main.go
+++ go-rest-api/main.go
@@ -2,32 +2,54 @@ package main
 
 import (
     "fmt"
+    "encoding/json"
     "net/http"
-    "io"
     "log"
+    "io"
 )
 
 func main(){
     
-
-    //http client
-
-    resp, err := http.Get("https://jsonplaceholder.typicode.com/users/1")
+    //creating a custom request
+    req, err := http.NewRequest("GET", "https://jsonplaceholder.typicode.com/users/1", nil)
 
     if err != nil {
-        log.Fatal("Error in response:", err)
+        log.Fatal("Error in creating a custom request: ", err)
+    }
+
+    //We can also add the header
+
+    req.Header.Set("Content-Type", "application/json")
+
+    //Executing the custom request
+
+    resp, err := http.DefaultClient.Do(req)
+
+    if err != nil {
+        log.Fatal("Error in responsing from server: ", err)
     }
 
     defer resp.Body.Close()
 
-    //reading the response
-
     body, err := io.ReadAll(resp.Body)
-
+    
     if err != nil {
-        log.Fatal("Error in reading the respoonse body: ", err)
+        log.Fatal("Error in converting response to body", err)
     }
+    
+    fmt.Println("response from server", string(body))
 
-    fmt.Println("Response from the server is ", string(body))
+    var result struct{
+            id int
+            name string
+            email string
+        }
+    
+
+        
+    if err := json.Unmarshal(body, &result); err != nil {
+      log.Fatal(err)
+    }
+    
+    fmt.Println("Response body is : ", result.id, result.name, result.email)
 }
-
